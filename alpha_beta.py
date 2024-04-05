6172390f93029c8b3e20d33f0aa1b02236f57749
Artemy <124582137+ArtemijsRadionovs@users.noreply.github.com>
2024-04-05 12:46:56 +0300

    Added alpha-beta algorithm for checking

---
+++ alpha_beta.py
@@ -0,0 +1,178 @@
+nodes = {
+    1: {'ind': 1, 'elem': 0, 'A': 50, 'B': 50, 'lvl': 0, 'rating': -1, 'childs': [2, 7, 12, 17, 22]},
+    
+        2: {'ind': 2, 'elem': 1, 'A': 50, 'B': 49, 'lvl': 1, 'rating': 1, 'childs': [3, 7, 11, 15]},
+        
+            3:{'ind': 3, 'elem': 2, 'A': 49, 'B': 48, 'lvl': 2, 'rating': -1, 'childs': [4, 5, 6]},
+                
+                4:{'ind': 4, 'elem': 3, 'A': 49, 'B': 47, 'lvl': 3, 'rating': 3, 'childs': []},
+                5:{'ind': 5, 'elem': 4, 'A': 48, 'B': 48, 'lvl': 3, 'rating': -1, 'childs': []},
+                6:{'ind': 6, 'elem': 5, 'A': 48, 'B': 47, 'lvl': 3, 'rating': 1, 'childs': []},
+        
+            7:{'ind': 7, 'elem': 3, 'A': 49, 'B': 49, 'lvl': 2, 'rating': 1, 'childs': [8, 9, 10]},
+                
+                8:{'ind': 8, 'elem': 2, 'A': 48, 'B': 48, 'lvl': 3, 'rating': 1, 'childs': []},
+                9:{'ind': 9, 'elem': 4, 'A': 48, 'B': 49, 'lvl': 3, 'rating': -1, 'childs': []},
+                10:{'ind': 10, 'elem': 5, 'A': 48, 'B': 48, 'lvl': 3, 'rating': 1, 'childs': []},
+            
+            11:{'ind': 11, 'elem': 4, 'A': 50, 'B': 48, 'lvl': 2, 'rating': -3, 'childs': [12, 13, 14]},
+                
+                12:{'ind': 12, 'elem': 2, 'A': 49, 'B': 47, 'lvl': 3, 'rating': 1, 'childs': []},
+                13:{'ind': 13, 'elem': 3, 'A': 50, 'B': 47, 'lvl': 3, 'rating': 3, 'childs': []},
+                14:{'ind': 14, 'elem': 5, 'A': 49, 'B': 47, 'lvl': 3, 'rating': 1, 'childs': []},
+            
+            15:{'ind': 15, 'elem': 5, 'A': 49, 'B': 48, 'lvl': 2, 'rating': -1, 'childs': [16, 17, 18]},
+                
+                16:{'ind': 16, 'elem': 2, 'A': 48, 'B': 47, 'lvl': 3, 'rating': 1, 'childs': []},
+                17:{'ind': 17, 'elem': 3, 'A': 49, 'B': 47, 'lvl': 3, 'rating': 3, 'childs': []},
+                18:{'ind': 18, 'elem': 4, 'A': 48, 'B': 48, 'lvl': 3, 'rating': -1, 'childs': []},
+        
+        19:{'ind': 19, 'elem': 2, 'A': 49, 'B': 49, 'lvl': 1, 'rating': -1, 'childs': [20, 24, 28, 32]},
+            
+            20:{'ind': 20, 'elem': 1, 'A': 48, 'B': 49, 'lvl': 2, 'rating': 1, 'childs': [21, 22, 23]},
+            
+                21:{'ind': 21, 'elem': 3, 'A': 48, 'B': 48, 'lvl': 3, 'rating': 1, 'childs': []},
+                22:{'ind': 22, 'elem': 4, 'A': 47, 'B': 49, 'lvl': 3, 'rating': -3, 'childs': []},
+                23:{'ind': 23, 'elem': 5, 'A': 47, 'B': 48, 'lvl': 3, 'rating': -1, 'childs': []},
+            
+            24:{'ind': 24, 'elem': 3, 'A': 48, 'B': 49, 'lvl': 2, 'rating': 1, 'childs': [25, 26, 27]},
+            
+                25:{'ind': 25, 'elem': 1, 'A': 48, 'B': 48, 'lvl': 3, 'rating': 1, 'childs': []},
+                26:{'ind': 26, 'elem': 4, 'A': 47, 'B': 49, 'lvl': 3, 'rating': -3, 'childs': []},
+                27:{'ind': 27, 'elem': 5, 'A': 47, 'B': 48, 'lvl': 3, 'rating': -1, 'childs': []},
+            
+            28:{'ind': 28, 'elem': 4, 'A': 49, 'B': 48, 'lvl': 2, 'rating': -3, 'childs': [29, 30, 31]},
+            
+                29:{'ind': 29, 'elem': 1, 'A': 49, 'B': 47, 'lvl': 3, 'rating': 1, 'childs': []},
+                30:{'ind': 30, 'elem': 3, 'A': 49, 'B': 47, 'lvl': 3, 'rating': 1, 'childs': []},
+                31:{'ind': 31, 'elem': 5, 'A': 48, 'B': 47, 'lvl': 3, 'rating': -1, 'childs': []},
+            
+            32:{'ind': 32, 'elem': 5, 'A': 48, 'B': 48, 'lvl': 2, 'rating': -1, 'childs': [33, 34, 35]},
+            
+                33:{'ind': 33, 'elem': 1, 'A': 48, 'B': 47, 'lvl': 3, 'rating': 1, 'childs': []},
+                34:{'ind': 34, 'elem': 3, 'A': 48, 'B': 47, 'lvl': 3, 'rating': 1, 'childs': []},
+                35:{'ind': 35, 'elem': 4, 'A': 47, 'B': 48, 'lvl': 3, 'rating': -3, 'childs': []},
+        
+        36:{'ind': 36, 'elem': 3, 'A': 50, 'B': 49, 'lvl': 1, 'rating': 1, 'childs': [37, 41, 45, 49]},
+        
+            37:{'ind': 37, 'elem': 1, 'A': 49, 'B': 49, 'lvl': 2, 'rating': 1, 'childs': [38, 39, 40]},
+        
+                38:{'ind': 38, 'elem': 2, 'A': 48, 'B': 48, 'lvl': 3, 'rating': 1, 'childs': []},
+                39:{'ind': 39, 'elem': 4, 'A': 48, 'B': 49, 'lvl': 3, 'rating': -1, 'childs': []},
+                40:{'ind': 40, 'elem': 5, 'A': 48, 'B': 48, 'lvl': 3, 'rating': 1, 'childs': []},
+        
+            41:{'ind': 41, 'elem': 2, 'A': 49, 'B': 48, 'lvl': 2, 'rating': -1, 'childs': [42, 43, 44]},
+        
+                42:{'ind': 42, 'elem': 1, 'A': 49, 'B': 47, 'lvl': 3, 'rating': 3, 'childs': []},
+                43:{'ind': 43, 'elem': 4, 'A': 48, 'B': 48, 'lvl': 3, 'rating': -1, 'childs': []},
+                44:{'ind': 44, 'elem': 5, 'A': 48, 'B': 47, 'lvl': 3, 'rating': 1, 'childs': []},
+        
+            45:{'ind': 45, 'elem': 4, 'A': 50, 'B': 48, 'lvl': 2, 'rating': -3, 'childs': [46, 47, 48]},
+        
+                46:{'ind': 46, 'elem': 1, 'A': 50, 'B': 47, 'lvl': 3, 'rating': 3, 'childs': []},
+                47:{'ind': 47, 'elem': 2, 'A': 49, 'B': 47, 'lvl': 3, 'rating': 1, 'childs': []},
+                48:{'ind': 48, 'elem': 5, 'A': 49, 'B': 47, 'lvl': 3, 'rating': 1, 'childs': []},
+        
+            49:{'ind': 49, 'elem': 5, 'A': 49, 'B': 48, 'lvl': 2, 'rating': -1, 'childs': [50, 51, 52]},
+        
+                50:{'ind': 50, 'elem': 1, 'A': 49, 'B': 47, 'lvl': 3, 'rating': 3, 'childs': []},
+                51:{'ind': 51, 'elem': 2, 'A': 48, 'B': 47, 'lvl': 3, 'rating': 1, 'childs': []},
+                52:{'ind': 52, 'elem': 4, 'A': 48, 'B': 48, 'lvl': 3, 'rating': -1, 'childs': []},
+        
+        53:{'ind': 53, 'elem': 4, 'A': 49, 'B': 50, 'lvl': 1, 'rating': -3, 'childs': [54, 58, 62, 66]},
+        
+            54:{'ind': 54, 'elem': 1, 'A': 48, 'B': 50, 'lvl': 2, 'rating': 1, 'childs': [55, 56, 57]},
+        
+                55:{'ind': 55, 'elem': 2, 'A': 47, 'B': 49, 'lvl': 3, 'rating': -3, 'childs': []},
+                56:{'ind': 56, 'elem': 3, 'A': 48, 'B': 49, 'lvl': 3, 'rating': -1, 'childs': []},
+                57:{'ind': 57, 'elem': 5, 'A': 47, 'B': 49, 'lvl': 3, 'rating': -3, 'childs': []},
+        
+            58:{'ind': 58, 'elem': 2, 'A': 48, 'B': 49, 'lvl': 2, 'rating': -1, 'childs': [59, 60, 61]},
+        
+                59:{'ind': 59, 'elem': 1, 'A': 48, 'B': 48, 'lvl': 3, 'rating': -1, 'childs': []},
+                60:{'ind': 60, 'elem': 3, 'A': 48, 'B': 48, 'lvl': 3, 'rating': -1, 'childs': []},
+                61:{'ind': 61, 'elem': 5, 'A': 47, 'B': 48, 'lvl': 3, 'rating': -3, 'childs': []},
+        
+            62:{'ind': 62, 'elem': 3, 'A': 48, 'B': 50, 'lvl': 2, 'rating': 1, 'childs': [63, 64, 65]},
+        
+                63:{'ind': 63, 'elem': 1, 'A': 48, 'B': 49, 'lvl': 3, 'rating': -1, 'childs': []},
+                64:{'ind': 64, 'elem': 2, 'A': 47, 'B': 49, 'lvl': 3, 'rating': -3, 'childs': []},
+                65:{'ind': 65, 'elem': 5, 'A': 47, 'B': 49, 'lvl': 3, 'rating': -3, 'childs': []},
+        
+            66:{'ind': 66, 'elem': 5, 'A': 48, 'B': 49, 'lvl': 2, 'rating': -1, 'childs': [67, 68, 69]},
+        
+                67:{'ind': 67, 'elem': 1, 'A': 48, 'B': 48, 'lvl': 3, 'rating': -1, 'childs': []},
+                68:{'ind': 68, 'elem': 2, 'A': 47, 'B': 48, 'lvl': 3, 'rating': -3, 'childs': []},
+                69:{'ind': 69, 'elem': 3, 'A': 48, 'B': 48, 'lvl': 3, 'rating': -1, 'childs': []},
+        
+        70:{'ind': 70, 'elem': 5, 'A': 49, 'B': 49, 'lvl': 1, 'rating': -1, 'childs': [71, 75, 79, 83]},
+        
+            71:{'ind': 71, 'elem': 1, 'A': 48, 'B': 49, 'lvl': 2, 'rating': 1, 'childs': [72, 73, 74]},
+
+                72:{'ind': 72, 'elem': 2, 'A': 47, 'B': 48, 'lvl': 3, 'rating': -1, 'childs': []},
+                73:{'ind': 73, 'elem': 3, 'A': 48, 'B': 48, 'lvl': 3, 'rating': 1, 'childs': []},
+                74:{'ind': 74, 'elem': 4, 'A': 47, 'B': 49, 'lvl': 3, 'rating': -3, 'childs': []},
+        
+            75:{'ind': 75, 'elem': 2, 'A': 48, 'B': 48, 'lvl': 2, 'rating': -1, 'childs': [76, 77, 78]},
+        
+                76:{'ind': 76, 'elem': 1, 'A': 48, 'B': 47, 'lvl': 3, 'rating': 1, 'childs': []},
+                77:{'ind': 77, 'elem': 3, 'A': 48, 'B': 47, 'lvl': 3, 'rating': 1, 'childs': []},
+                78:{'ind': 78, 'elem': 4, 'A': 47, 'B': 48, 'lvl': 3, 'rating': -3, 'childs': []},
+        
+            79:{'ind': 79, 'elem': 3, 'A': 48, 'B': 49, 'lvl': 2, 'rating': 1, 'childs': [80, 81, 82]},
+        
+                80:{'ind': 80, 'elem': 1, 'A': 48, 'B': 48, 'lvl': 3, 'rating': 1, 'childs': []},
+                81:{'ind': 81, 'elem': 2, 'A': 47, 'B': 48, 'lvl': 3, 'rating': -1, 'childs': []},
+                82:{'ind': 82, 'elem': 4, 'A': 47, 'B': 49, 'lvl': 3, 'rating': -3, 'childs': []},
+        
+            83:{'ind': 83, 'elem': 4, 'A': 49, 'B': 48, 'lvl': 2, 'rating': -3, 'childs': [84, 85, 86]},
+        
+                84:{'ind': 84, 'elem': 1, 'A': 49, 'B': 47, 'lvl': 3, 'rating': 1, 'childs': []},
+                85:{'ind': 85, 'elem': 2, 'A': 48, 'B': 47, 'lvl': 3, 'rating': -1, 'childs': []},
+                86:{'ind': 86, 'elem': 3, 'A': 49, 'B': 47, 'lvl': 3, 'rating': 1, 'childs': []},
+}
+
+def alpha_beta(curr, alpha, beta, isMaxFlag = True):
+    global nodes
+    theNode = nodes[curr]
+    # print(">>>>> Enter with: " + str(curr))
+    # print()
+    # print(theNode)
+    locElem = 0
+    locIndex = 0
+    locRating = float('-inf')
+    if len(theNode['childs']) > 0:
+        firstChildFlag = True
+        for childInd in theNode['childs']:
+            childElem, childIndex, childRating = alpha_beta(childInd, alpha, beta, not isMaxFlag)
+            if firstChildFlag:
+                # print("    Inside " + str(theNode['ind']) + " from loc " + str(locRating)+ " to child " + str(childRating))
+                firstChildFlag = False
+                locElem = childElem
+                locIndex = childInd
+                locRating = childRating
+            else:
+                if isMaxFlag:
+                    if locRating < childRating: # Find max value. If yes, save.
+                        locElem = childElem
+                        locIndex = childInd
+                        locRating = childRating
+                    alpha = max(alpha, locRating)
+                    if beta <= alpha:
+                        break
+                else:
+                    if locRating > childRating: # Find min value. If yes, save.
+                        locElem = childElem
+                        locIndex = childInd
+                        locRating = childRating
+                    beta = min(beta, locRating)
+                    if beta <= alpha:
+                        break
+    else:
+        locElem = theNode['elem']
+        locIndex = theNode['ind']
+        locRating = theNode['rating']
+    # print("<<<<< Exit with: " + str(theNode['ind']) + " | " + str(locElem) + " | " + str(locRating))
+    theNode['ab_rating'] = locRating
+    theNode['minF_maxT'] = isMaxFlag
+    return locElem, locIndex, locRating
