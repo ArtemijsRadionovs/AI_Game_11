c9c590de87b14ecf15100bd33281cd557cbc3446
Artemy <124582137+ArtemijsRadionovs@users.noreply.github.com>
2024-04-08 21:27:12 +0300

    Final ai_game.py

--- ai_game.py
+++ ai_game.py
@@ -1,23 +1,24 @@
+# Importēt nepieciešamās bibliotēkas un moduļus
 import pygame
 import sys
 import random
 from tree_logic import selectMinMax, selectAlphaBeta, growBranch, getInd, nodes
 import time
 
-# Static color variables
+# Statiskie krāsu mainīgie
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 RED = (255, 0, 0)
 DARK_YELLOW = (255, 215, 0)
 PATH = []
 
-# Define Player class
+# Definēt spēlētāja klasi
 class Player:
     def __init__(self, name, score):
         self.name = name
         self.score = score
 
-# Function to generate array
+# Funkcija, lai ģenerētu masīvu
 def generate_array(length):
     value_array = {}
 
@@ -28,7 +29,7 @@ def generate_array(length):
     print(value_array)
     return value_array
 
-# Function to print players' scores
+# Funkcija, lai izdrukātu spēlētāju rezultātus
 def print_players_score(screen, players):
     font = pygame.font.SysFont(None, 24)
     for i, player in enumerate(players):
@@ -36,7 +37,7 @@ def print_players_score(screen, players):
         screen.blit(text, (20, 20 + i * 30))
 
 
-# Function to display whose turn it is
+# Funkcija, lai parādītu, kura spēlētāja kārta ir
 def display_turn(screen, current_player):
     font = pygame.font.SysFont(None, 24)
     if current_player.name == "Player":
@@ -46,8 +47,7 @@ def display_turn(screen, current_player):
     text_rect = text.get_rect(topright=(screen.get_width() - 20, 20))
     screen.blit(text, text_rect)
 
-
-# Function to display input box for array length
+# Funkcija, lai parādītu ievades lodziņu masīva garumam
 def enter_val_arr_len(screen):
     input_text = ""
     error_message = ""
@@ -83,8 +83,7 @@ def enter_val_arr_len(screen):
         input_surface = font.render(input_text, True, BLACK)
         screen.blit(input_surface, (text_rect.centerx - input_surface.get_width() // 2, text_rect.centery + 50))
 
-        
-        # Display error message
+        # Parādīt kļūdas ziņojumu
         if error_message:
             error_font = pygame.font.SysFont(None, 35)
             error_text = error_font.render(error_message, True, RED)
@@ -93,8 +92,7 @@ def enter_val_arr_len(screen):
         
         pygame.display.flip()
             
-            
-# Function to analyze win and return winner's name
+# Funkcija, lai analizētu uzvaru un atgrieztu uzvarētāja vārdu
 def analyze_win(players):
     if players[0].score > players[1].score:
         return players[0].name
@@ -103,7 +101,7 @@ def analyze_win(players):
     else:
         return "Draw"
 
-# Function to choose starter
+# Funkcija, lai izvēlētos, kurš sāks spēli
 def choose_starter(screen):
     starter_choice = None
     starter_choice_screen = pygame.Surface(screen.get_size())
@@ -119,25 +117,25 @@ def choose_starter(screen):
     button_height = 50
     button_margin = 10
 
-    # Calculate the total height of the buttons and the text
+    # Aprēķināt pogu un teksta kopējo augstumu
     total_height = button_height * 2 + button_margin
 
-    # Calculate the starting y position for the buttons and text
+    # Aprēķināt pogas un teksta sākuma y pozīciju
     start_y = (starter_choice_screen.get_height() - total_height) // 2
 
     button_x = (screen.get_width() - button_width) // 2
 
-    # Adjust the position of the text and buttons
+    # Teksta un pogu pozīcijas pielāgošana
     text_rect_starter_choice.y = start_y
 
-    # Button "Player"
+    # Poga "Player"
     button_y_player = start_y + text_rect_starter_choice.height + button_margin
     text_player_button = button_font.render("Player", True, WHITE)
     text_rect_player_button = text_player_button.get_rect(center=(button_x + button_width // 2, button_y_player + button_height // 2))
     pygame.draw.rect(starter_choice_screen, RED, (button_x, button_y_player, button_width, button_height))
     starter_choice_screen.blit(text_player_button, text_rect_player_button)
 
-    # Button "Computer"
+    # Poga "Computer"
     button_y_computer = button_y_player + button_height + button_margin
     text_computer_button = button_font.render("Computer", True, WHITE)
     text_rect_computer_button = text_computer_button.get_rect(center=(button_x + button_width // 2, button_y_computer + button_height // 2))
@@ -161,41 +159,41 @@ def choose_starter(screen):
 
     return starter_choice
 
-# Function to choose algorithm
+# Funkcija, lai izvēlētos algoritmu
 def choose_algorithm(screen):
     algorithm_choice = None
     algorithm_choice_screen = pygame.Surface(screen.get_size())
     algorithm_choice_screen.fill(WHITE)
     
-    # Text "Choose algorithm:"
+    # Teksts "Choose algorithm:"
     font = pygame.font.SysFont(None, 36)
     text_algorithm_choice = font.render("Choose algorithm:", True, BLACK)
     text_rect_algorithm_choice = text_algorithm_choice.get_rect(center=algorithm_choice_screen.get_rect().center)
     text_rect_algorithm_choice.y -= 55
     algorithm_choice_screen.blit(text_algorithm_choice, text_rect_algorithm_choice)
     
-    # Button parameters
+    # Pogas parametri
     button_font = pygame.font.SysFont(None, 30)
     button_width = 145
     button_height = 50
     button_margin = 20
-    button_padding = 15  # Added padding between buttons
+    button_padding = 15  # Pievienota atkāpe starp pogām
     
-    # Buttons position
+    # Poga posicija
     button_x = (algorithm_choice_screen.get_width() - button_width) // 2
 
     button_y_minmax = text_rect_algorithm_choice.bottom + button_margin
     button_y_alphabeta = button_y_minmax + button_height + button_padding  # Adjusted position
     
-    # Button "MinMax"
+    # Poga "MinMax"
     text_minmax_button = button_font.render("MinMax", True, WHITE)
     text_rect_minmax_button = text_minmax_button.get_rect(center=(button_x + button_width // 2, button_y_minmax + button_height // 2))
 
-    # Button "Alpha-beta"
+    # Poga "Alpha-beta"
     text_alphabeta_button = button_font.render("Alpha-beta", True, WHITE)
     text_rect_alphabeta_button = text_alphabeta_button.get_rect(center=(button_x + button_width // 2, button_y_alphabeta + button_height // 2))
 
-    # Blit text and buttons to the screen
+    # Teksta un pogu parādīšana uz ekrāna
     pygame.draw.rect(algorithm_choice_screen, RED, (button_x, button_y_minmax, button_width, button_height))
     algorithm_choice_screen.blit(text_minmax_button, text_rect_minmax_button)
     pygame.draw.rect(algorithm_choice_screen, RED, (button_x, button_y_alphabeta, button_width, button_height))  # Adjusted position
@@ -204,7 +202,6 @@ def choose_algorithm(screen):
     screen.blit(algorithm_choice_screen, (0, 0))
     pygame.display.flip()
 
-    # Event handling
     while not algorithm_choice:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -219,24 +216,24 @@ def choose_algorithm(screen):
 
     return algorithm_choice
 
-# Function to draw squares with numbers
+# Funkcija, lai uzzīmētu kvadrātus ar skaitļiem
 def draw_squares(screen, squares):
     font = pygame.font.SysFont(None, 50)
-    square_rects = {}  # Dictionary to store square rectangles
+    
+    square_rects = {}  # Vārdnīca, lai glabātu kvadrātu taisnstūrus
     for i, (x, value) in enumerate(squares.items(), start=1):
         square_text = font.render(str(value), True, DARK_YELLOW)
         square_rect = square_text.get_rect(center=(x * 35, 120))
         pygame.draw.rect(screen, RED, square_rect)
         screen.blit(square_text, square_rect)
-        square_rects[x] = square_rect  # Store rectangle in dictionary with square number as key
+        
+        square_rects[x] = square_rect  # Glabāt kvadrātu taisnstūrus vārdnīcā ar kvadrāta numuru kā atslēgu
     return square_rects
 
-# Function to run the chosen algorithm
+# Funkcija, kas palaiž algoritmu, lai izvēlētos kvadrātu
 def computer_use_algorithm(algorithm_choice, ind):
-    # Chosen algorithm
     
     if algorithm_choice == "MinMax":
-        # selectMinMax(ind, True)
         tree_elem, tree_ind, tree_rating = selectMinMax(ind, True)
         best_node_ind = nodes[tree_ind]['ind']
         print("Best node:", best_node_ind)
@@ -249,7 +246,6 @@ def computer_use_algorithm(algorithm_choice, ind):
         return square, lvl, best_node_ind
         
     elif algorithm_choice == "Alpha-beta":
-        # selectAlphaBeta(ind, float('-inf'), float('inf'), True)
         tree_elem, tree_ind, tree_rating = selectAlphaBeta(ind, float('-inf'), float('inf'), True)
         best_node_ind = nodes[tree_ind]['ind']
         print("Best node:", best_node_ind)
@@ -261,9 +257,8 @@ def computer_use_algorithm(algorithm_choice, ind):
 
         return square, lvl, best_node_ind
 
-    # Initialize the global variables
 
-# Function to update player's score based on the square chosen
+# Funkcija, lai atjaunotu spēlētāja rezultātu
 def player_score_update(players, current_player, game_array, square):
     arr_val = game_array[square]
     if arr_val == 1:
@@ -278,10 +273,16 @@ def player_score_update(players, current_player, game_array, square):
         players[1 - players.index(current_player)].score -= 1
     
     PATH.append((square, arr_val))
-    game_array.pop(square)  # Remove the clicked square from the array
+    game_array.pop(square)  # Nodzēst izvēlēto kvadrātu no spēles masīva
 
-# Function to run the game
+# Funkcija, lai palaistu izvēlēto algoritmu
 def run_game(screen):
+    """
+    Palaist spēles ciklu.
+
+    Argumenti:
+        screen: Pygame ekrāna objekts.
+    """
     player1 = Player("Player", 50)
     player2 = Player("Computer", 50)
     players = [player1, player2]
@@ -313,59 +314,59 @@ def run_game(screen):
             if event.type == pygame.QUIT:
                 running = False
                 ind = 0
-                return 'No one' # Return 'quit' to exit the game
+                return 'No one' # Atgriezt 'quit', lai izietu no spēles
 
             elif event.type == pygame.MOUSEBUTTONDOWN:
                 mouse_pos = pygame.mouse.get_pos()
                 for square, square_rect in square_rects.items():
-
-                    if square_rect.collidepoint(mouse_pos):  # Check if mouse click occurred within the square button
-                        if square in game_array:  # Check if the square number exists in the array
+                    
+                    if square_rect.collidepoint(mouse_pos):  # Pārbaudīt, vai notika peles klikšķis kvadrāta pogā
+                        if square in game_array:  # Pārbaudīt, vai kvadrāts ir spēles masīvā
                             if current_player.name == "Player":
                                 
-                                player_score_update(players, current_player, game_array, square) # Update player's score
+                                player_score_update(players, current_player, game_array, square) # Atjaunot spēlētāja rezultātu
                                 
                                 element = nodes[ind]['elem']
                                 if element == square:
                                     ind = nodes['ind']
                                 
                                 growBranch(ind, square, game_array, player1.score, player2.score, 0)
-
-                                current_player = players[1 - players.index(current_player)]  # Switch turn to the other player
+                                
+                                current_player = players[1 - players.index(current_player)]  # Izmainīt gājienu uz otru spēlētāju
 
                                 if current_player.name == "Computer":
-                                    if game_array:  # Check if the game array is not empty
+                                    if game_array:  # Pārbaudīt, vai spēles masīvs nav tukšs
                                         start_time = time.time()
                                         square, lvl, best_node_ind = computer_use_algorithm(algorithm_choice, ind)
 
-                                        player_score_update(players, current_player, game_array, square) # Update player's score
+                                        player_score_update(players, current_player, game_array, square) # Atjaunot spēlētāja rezultātu
 
                                         end_time = time.time()
                                         print("Time taken for turn:", end_time - start_time)
                                         
                                         growBranch(best_node_ind, square, game_array, player1.score, player2.score, lvl)
 
-                                        current_player = players[1 - players.index(current_player)]  # Switch turn to the other player
+                                        current_player = players[1 - players.index(current_player)]  # Izmainīt gājienu uz otru spēlētāju
                             
                             elif current_player.name == "Computer":
                                 start_time = time.time()
                                 square, lvl, best_node_ind = computer_use_algorithm(algorithm_choice, ind)
                                 
-                                player_score_update(players, current_player, game_array, square) # Update player's score
+                                player_score_update(players, current_player, game_array, square) # Atjaunot spēlētāja rezultātu
 
                                 end_time = time.time()
                                 print("Time taken for turn:", end_time - start_time)
                                 
                                 growBranch(best_node_ind, square, game_array, player1.score, player2.score, lvl)
 
-                                current_player = players[1 - players.index(current_player)]  # Switch turn to the other player
+                                current_player = players[1 - players.index(current_player)]  # Izmainīt gājienu uz otru spēlētāju
                                 
 
         game_screen = pygame.Surface(screen.get_size())
         game_screen.fill(WHITE)
         print_players_score(game_screen, players)
-        draw_squares(game_screen, game_array)  # Draw squares with numbers
-        display_turn(game_screen, current_player)  # Display whose turn it is
+        draw_squares(game_screen, game_array)  # Uzzīmēt kvadrātus ar skaitļiem
+        display_turn(game_screen, current_player)  # Parādīt, kvadrātus ekranā
         screen.blit(game_screen, (0, 0))
         pygame.display.flip()
 
@@ -377,7 +378,7 @@ def run_game(screen):
     pygame.display.flip()
 
     winner = analyze_win(players)
-    return winner if winner != 'Draw' else 'draw'  # Return 'draw' if it's a tie
+    return winner if winner != 'Draw' else 'draw'  # Atgriezt uzvarētāju, ja nav neizšķirta
 
 
 def display_winner(screen, winner):
@@ -394,7 +395,6 @@ def display_winner(screen, winner):
     text_rect_winner = text_winner.get_rect(center=screen.get_rect().center)
     screen.blit(text_winner, text_rect_winner)
     
-    #if winner != 'draw':
     font_prompt = pygame.font.SysFont(None, 24)
     text_prompt = font_prompt.render("Press R to restart the game or Q to quit", True, BLACK)
     text_rect_prompt = text_prompt.get_rect(center=(screen.get_width() // 2, screen.get_height() - 50))
@@ -415,7 +415,7 @@ def display_winner(screen, winner):
                     sys.exit()
 
 
-# Main function
+# Galvenās spēles funkcija, kur tiek inicializēts Pygame un palaista spēle
 def main():
     pygame.init()
     WIDTH, HEIGHT = 1000, 600
@@ -464,7 +464,7 @@ def main():
                 elif action == 'quit':
                     pygame.quit()
                     sys.exit()
-        # After the game loop breaks, reset the click flag to False
+        # Pēc spēles cikla pārtraukšanas atiestatīt klikšķa karodziņu uz False
             start_clicked = False
                     
 
